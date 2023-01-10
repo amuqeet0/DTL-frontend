2e361635898004ddeae9497f5330df4daed1c9eb
amuqeet0 <111070408+amuqeet0@users.noreply.github.com>
2023-01-10 18:18:15 +0500

    styling guideline added

--- src/components/shared/stylingExample/index.tsx
+++ src/components/shared/stylingExample/index.tsx
@@ -1,15 +1,37 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
+import { makeStyles } from 'tss-react/mui'
 // import { useTheme } from "@mui/material/styles";
 
-export default function Home() {
+export default function StyledComp() {
   // const theme = useTheme();
+  // const { classes, cx } = useStyles({ "color": isClicked ? "blue": "red" });
+  const { classes } = useStyles()
   return (
-    <div>
-      <Box component="form">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </Box>
-    </div>
+    <Box component="form" className={classes.root}>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, porro.
+      </p>
+    </Box>
   )
 }
+
+const useStyles = makeStyles()({
+  //If you don't need neither the theme nor any state or
+  //props to describe your component style you can pass-in
+  //an object instead of a callback.
+  root: {
+    backgroundColor: 'pink',
+  },
+})
+
+// const useStyles = makeStyles()(
+//   (theme) => ({
+//       "root": {
+//           color,
+//           "&:hover": {
+//               "color": theme.palette.primary.main
+//           }
+//       }
+//   })
+// );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import StyledComp from '../components/shared/stylingExample'
 
 export default function Home() {
   return (
     <div>
       <Box component="form" noValidate autoComplete="off">
+        <StyledComp />
         <TextField
           className="bg-blue"
           id="outlined-basic"
